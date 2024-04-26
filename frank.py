8169157ffc7e8a5ef41f1f30479e235b3fce448a
Frank <f.r.harraway@student.tudelft.nl>
2024-04-26 14:09:42 +0200

    Got transmission loss working on RG and PDMM

--- frank.py
+++ frank.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
 
-np.random.seed(1)
+# np.random.seed(1)
 
 
 def build_random_graph(required_probability=0.999):
@@ -110,17 +110,57 @@ def random_gossip(temperature, A, tolerance=0.00001):
     return loss,temperature
 
 
-def PDMM_sync(temperature, G,tolerance=10**-8,c=0.3):
-    num_nodes = G.number_of_nodes()
-    x=np.zeros([num_nodes,1])
-    incidence_matrix = nx.incidence_matrix(G).toarray().T
-    A_ij=dict()
+def random_gossip_TF(temperature, A, tolerance=0.00001, transmission_failure=0.1):
+    """
+    Perform random gossip algorithm to update the temperature values.
 
+    Parameters:
+    temperature (numpy.ndarray): The initial temperature values for each node.
+    num_nodes (int): The total number of nodes in the network.
+    A (numpy.ndarray): The adjacency matrix representing the network connections.
+    tolerance (float, optional): The convergence tolerance. Defaults to 0.00001.
 
+    Returns:
+    numpy.ndarray: The loss values at each iteration until convergence.
+
+    """
+    num_nodes = np.shape(A)[0]
     converged = False
     loss = np.array([])
     avg_temp = np.mean(temperature)
+    while not converged:
+        #transmission failure
+        if np.random.uniform(low=0, high=1)<transmission_failure:
+            loss= np.append(loss, np.sum((temperature - avg_temp)**2))
+            continue
+        node_i = int(np.random.uniform(low=0, high=num_nodes))
+        i_neigh = np.transpose(np.nonzero(A[node_i, :]))
+        j_index = int(np.random.uniform(low=0, high=np.shape(i_neigh)[0]))
+        node_j = i_neigh[j_index][0]
 
+        # update equation
+        W = W_construct_rand_gossip(node_i, node_j, num_nodes)
+        temperature = np.dot(W, temperature)
+        # avg=(temperature[node_i]+temperature[node_j])/2
+        # temperature[node_i]=avg
+        # temperature[node_j]=avg
+
+        if np.sum((temperature - avg_temp)**2)< tolerance:
+            loss = np.append(loss, np.sum((temperature - avg_temp)**2))
+            converged = True
+        else:
+            loss= np.append(loss, np.sum((temperature - avg_temp)**2))
+    return loss,temperature
+
+
+
+def PDMM_async(temperature, G,tolerance=10**-8,c=0.3):
+    num_nodes = G.number_of_nodes()
+    x=np.zeros([num_nodes,1])
+    A_ij=dict()
+    converged = False
+    loss = np.array([])
+    avg_temp = np.mean(temperature)
     #initialise A_ij
     for edge in G.edges:
         if edge[0] < edge[1]:
@@ -165,20 +205,70 @@ def PDMM_sync(temperature, G,tolerance=10**-8,c=0.3):
         for j in G.neighbors(i):
             z[i,j]=y[j,i]
                 
-        
-        
-
-    
     return loss,transmissions
 
+def PDMM_async_TF(temperature, G, tolerance=10**-8, c=0.3, transmission_failure=0.1):
+    num_nodes = G.number_of_nodes()
+    x = np.zeros([num_nodes, 1])
+    A_ij = dict()
+    converged = False
+    loss = np.array([])
+    avg_temp = np.mean(temperature)
 
-def PDMM_async(temperature, G,tolerance=10**-8,c=0.1):
+    # Initialise A_ij
+    for edge in G.edges:
+        if edge[0] < edge[1]:
+            A_ij[edge[0], edge[1]] = 1
+            A_ij[edge[1], edge[0]] = -1
+        else:
+            A_ij[edge[0], edge[1]] = -1
+            A_ij[edge[1], edge[0]] = 1
+
+    # Initialise z_ij and y_ij
+    z = dict()
+    y = dict()
+    for i in np.arange(0, num_nodes):
+        for j in G.neighbors(i):
+            z[i, j] = 0
+            y[i, j] = 0
+
+    transmissions = []
+    tot_transmissions = 0
+    while not converged:
+        i = int(np.random.uniform(low=0, high=num_nodes))
+        if np.random.uniform(low=0, high=1) < transmission_failure:
+            tot_transmissions += 1
+            transmissions.append(tot_transmissions)
+            loss = np.append(loss, np.sum((x - avg_temp)**2))
+            continue
+
+        # Update x_i
+        x[i] = temperature[i]
+        for j in G.neighbors(i):
+            x[i] -= A_ij[i, j] * z[i, j]
+        x[i] /= (1 + c * G.degree(i))
+
+        # Update y_ij
+        for j in G.neighbors(i):
+            y[i, j] = z[i, j] + 2 * c * (x[i] * A_ij[i, j])
+
+        tot_transmissions += 1
+        transmissions.append(tot_transmissions)
+        loss = np.append(loss, np.sum((x - avg_temp)**2))
+
+        if np.sum((x - avg_temp)**2) < tolerance:
+            converged = True
+
+        # Update z_ij
+        for j in G.neighbors(i):
+            z[i, j] = y[j, i]
+
+    return loss, transmissions
+
+def PDMM_sync(temperature, G,tolerance=10**-8,c=0.1):
     num_nodes = G.number_of_nodes()
     x=np.zeros([num_nodes,1])
-    incidence_matrix = nx.incidence_matrix(G).toarray().T
     A_ij=dict()
-
-
     converged = False
     loss = np.array([])
     avg_temp = np.mean(temperature)
@@ -234,7 +324,6 @@ def PDMM_async(temperature, G,tolerance=10**-8,c=0.1):
 
     
     return loss,transmissions
-
 
 def async_distr_averaging(temperature,A,tolerance):
     """
@@ -307,16 +396,25 @@ tolerance=10**-12
 # plt.show()
 c=0.3
 
-loss_pdmm_sync,trans_pdmm_sync=PDMM_sync(temperature,G,tolerance,c)
-loss_pdmm_async,trans_pdmm_async=PDMM_sync(temperature,G,tolerance,c)
-loss_random,temperature_rand=random_gossip(temperature.copy(),A,tolerance)
+# loss_random,temperature_rand=random_gossip(temperature.copy(),A,tolerance)
+# loss_random_tf,temperature_rand_tf=random_gossip_TF(temperature.copy(),A,tolerance,0.5)
+# plot_log_convergence([loss_random_tf, loss_random],[np.arange(1,loss_random_tf.shape[0]+1),np.arange(1,loss_random.shape[0]+1)],['Random Gossip TF',"Random gossip"],num_nodes)
 
-loss_async,trans_async, temperature_async =async_distr_averaging(temperature.copy(),A,tolerance)
+loss_pdmm_async_tf,trans_pdmm_async_tf=PDMM_async_TF(temperature,G,tolerance,c,0.5)
+loss_pdmm_async,trans_pdmm_async=PDMM_async(temperature,G,tolerance,c)
+plot_log_convergence([loss_pdmm_async,loss_pdmm_async_tf],[trans_pdmm_async,trans_pdmm_async_tf],['PDMM_Async ',"PDMM_Async_TF"],num_nodes)
 
 
-plot_log_convergence([loss_async,loss_random,loss_pdmm_sync,loss_pdmm_async],[trans_async,np.arange(1,loss_random.shape[0]+1),trans_pdmm_sync,trans_pdmm_async],['Asynchronous Distributed Averaging','Random Gossip','Synchronous PDMM',"Asynchronous PDMM"],num_nodes)
+# loss_pdmm_sync,trans_pdmm_sync=PDMM_sync(temperature.copy(),G,tolerance,c)
+# loss_pdmm_async,trans_pdmm_async=PDMM_async(temperature.copy(),G,tolerance,c)
+# loss_random,temperature_rand=random_gossip(temperature.copy(),A,tolerance)
+
+# loss_async,trans_async, temperature_async =async_distr_averaging(temperature.copy(),A,tolerance)
 
 
-#select a node i at random (uniformly) and contact neigbouring node j at random(uniformly)
-nx.draw(G, pos=pos)
-plt.show()
+# plot_log_convergence([loss_async,loss_random,loss_pdmm_sync,loss_pdmm_async],[trans_async,np.arange(1,loss_random.shape[0]+1),trans_pdmm_sync,trans_pdmm_async],['Asynchronous Distributed Averaging','Random Gossip','Synchronous PDMM',"Asynchronous PDMM"],num_nodes)
+
+
+# #select a node i at random (uniformly) and contact neigbouring node j at random(uniformly)
+# nx.draw(G, pos=pos)
+# plt.show()
